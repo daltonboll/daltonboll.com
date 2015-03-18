f9cfcc3b225528b7d41c2f25f27a330186206085
daltonboll <daltonboll@gmail.com>
2015-03-18 05:48:46 +0000

    Added automatic fixed image dimensions embedded into html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
   
+  # Automatically add image dimensions imbedded into HTML (i.e. for logo, width="200", height="35")
+  def image_tag(source, options = {})
+    if !options[:size] # Do not overwrite size if already set by caller
+      @@image_size ||= {} # Cache for image sizes
+      if !@@image_size[source] # Fill cache
+        @@image_size[source] = FastImage.size(::Rails.root.to_s+"/app/assets/images/#{source}", :raise_on_failure => true).join("x")
+      end
+      options = options.merge(:size => @@image_size[source])
+    end
+    super(source, options)
+  end
+  
 end
