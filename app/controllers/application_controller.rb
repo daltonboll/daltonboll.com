5f70a55959d1f93dbb28889aea4d39068b58a72a
daltonboll <daltonboll@gmail.com>
2015-03-18 07:37:18 +0000

    Added 301 redirect from heroku domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Ensure 301 redirect from heroku
+  before_filter :ensure_domain
+  APP_DOMAIN = 'www.daltonboll.com'
+
+  # Ensure 301 redirect from heroku
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
 end
