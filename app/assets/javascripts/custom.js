6856e929c0a94b0cf7b88674eeb2741445ba005c
Dalton Boll <daltonboll@gmail.com>
2015-03-24 02:48:07 -0700

    Removed turbolinks to fix footer javascript fix interference

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,10 @@
+// Fix for footer floating to middle of page when there isn't enough content to show
+$(document).ready(function() {
+   var docHeight = $(window).height();
+   var footerHeight = $('#footer').height();
+   var footerTop = $('#footer').position().top + footerHeight;
+
+   if (footerTop < docHeight) {
+    $('#footer').css('margin-top', 10+ (docHeight - footerTop) + 'px');
+   }
+});
