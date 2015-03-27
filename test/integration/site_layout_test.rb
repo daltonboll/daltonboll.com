a052de547d259678797700234f0a817cc295477f
Dalton Boll <daltonboll@gmail.com>
2015-03-26 21:18:26 -0700

    Fixed site layout tests that were failing. Changed 'content' on mail form to 'message'

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,8 +4,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
     get root_path
-    assert_template 'pages/home'
+    assert_template 'pages/about'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
   end
 end
