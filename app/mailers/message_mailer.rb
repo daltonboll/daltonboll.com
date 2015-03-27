6669b08d1b1ce27517cbb2a7f3aac35e8dcf2ea3
Dalton Boll <daltonboll@gmail.com>
2015-03-26 20:11:04 -0700

    Fixed Mailer/Message test cases

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,2 +1,10 @@
 class MessageMailer < ApplicationMailer
+  # This will be the email address that the messages will be sent to by default
+  default :to => "daltonboll@gmail.com"
+
+  def message_me(msg)
+  	@msg = msg
+
+  	mail from: @msg.email, subject: @msg.subject, body: @msg.content
+  end
 end
--- test/mailers/message_mailer_test.rb
+++ test/mailers/message_mailer_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class MessageMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "message me" do
+
+    msg = Message.new(name: 'Annie',
+      email: 'annie@example.com',
+      subject: 'Guten Tag!',
+      content: 'Auf Wiedersehen!')
+
+    email = MessageMailer.message_me(msg).deliver_now
+
+    refute ActionMailer::Base.deliveries.empty? 
+    assert_equal ['daltonboll@gmail.com'], email.to
+    assert_equal ['annie@example.com'], email.from
+    assert_equal 'Guten Tag!', email.subject
+    assert_equal 'Auf Wiedersehen!', email.body.to_s
+  end
 end
