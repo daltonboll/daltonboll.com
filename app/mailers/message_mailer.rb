b5be9143a93328de329b7f438ed777cb3c45106c
Dalton Boll <daltonboll@gmail.com>
2015-03-26 22:51:37 -0700

    Now using an html file as the template for message notifications to the admin

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,15 +1,11 @@
 class MessageMailer < ApplicationMailer
   # This will be the email address that the messages will be sent to by default
   # default :to => "daltonboll@gmail.com"
-  $to_me = "daltonboll@gmail.com"
-  $from_me = "daltonboll@gmail.com"
+  $my_email = "daltonboll@gmail.com"
   
-
   def message_me(msg)
   	@msg = msg
-  	new_content = "New message received from #{@msg.name} at #{@msg.email}:\n----------\n\"#{@msg.content}\"\n----------"
-
-  	mail to: $to_me, from: @msg.email, subject: @msg.subject, body: new_content
+  	mail to: $my_email, from: @msg.email, subject: @msg.subject, reply_to: @msg.email
   end
 
   def notify_sender(msg)
@@ -18,8 +14,9 @@ class MessageMailer < ApplicationMailer
   	" a copy of your message is displayed below:\n----------\n\"#{@msg.content}\"\n----------\n\nI will do my best to get back to "\
   	"you as soon as possible. In the meantime, why not check out my blog?\n\nBest,\nDalton Boll\ndaltonboll.com"
 
-  	@notify_subject = "Thank you for leaving me a message at daltonboll.com"
+  	@notify_subject = "Message received"
+  	@from_name = "Dalton Boll"
 
-  	mail to: @msg.email, from: $from_me, subject: @notify_subject, body: new_content
+  	mail to: @msg.email, from: @from_name, subject: @notify_subject, body: new_content
   end
 end
