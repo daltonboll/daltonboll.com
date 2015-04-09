678b630838ebcb50dfe09af2f63a20a106ed1a44
Dalton Boll <daltonboll@gmail.com>
2015-04-08 23:32:49 -0700

    Fix before_filter :social_buttons and :footer_links in Comments Controller and Posts Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,8 @@ class CommentsController < ApplicationController
   include PagesHelper
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_filter :header_links # Allow @header_links from PagesHelper to be accessible
-  before_filter :footer_links # Allow @footer_links from PagesHelper to be accessible
-
+  before_filter :social_buttons # Allow @social_buttons from PagesHelper to be accessible
+  
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(params[:comment]) #TODO: get rid of !
