62fe409eda553c231fd1f6a33af6e1db32771163
Dalton Boll <daltonboll@gmail.com>
2015-04-09 19:47:10 -0700

    Add links to Comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  def get_formatted_website
+    site = self.website
+    if self.website
+      if site.start_with?("http://") or site.start_with?("https://")
+        return site
+      else
+        return site = "http://#{site}"
+      end
+    else
+      return "#"
+    end
+  end
+
 end
